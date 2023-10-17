c441e52d9ccd496d9f9571d5673aa87ea47b6d1e
wesi4a <veselinak1510@gmail.com>
2023-10-17 18:10:28 +0300

    AbstractFactory

---
+++ src/AbstractFactoryPatternDemo.java
@@ -0,0 +1,12 @@
+public class AbstractFactoryPatternDemo {
+    public static void main(String[] args) {
+        //get shape factory
+        AbstractFactory shapeFactory = FactoryProducer.getFactory("SHAPE");
+
+        //get an object of Shape Circle
+        Shape shape1 = shapeFactory.getShape("CIRCLE");
+
+        //call draw method of Shape Circle
+        shape1.draw();
+    }
+}
